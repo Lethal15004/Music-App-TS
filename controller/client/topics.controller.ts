77c3c64100649214a59babd8d1ab00b2a6a2eb6c
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-14 18:50:47 +0700

    Làm trang Chủ đề bài hát (bên client)

--- controller/client/topics.controller.ts
+++ controller/client/topics.controller.ts
@@ -4,6 +4,7 @@ import Topic from '../../model/topics.model';
 export const index = async(req: Request, res:Response)=>{
     const topics = await Topic.find({deleted: false});
     res.render('client/pages/topics/index', {
-        title:'Danh sách chủ đề'
+        title:'Danh sách chủ đề',
+        topics:topics
     })
 }
