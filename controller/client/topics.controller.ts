200d84e0bd4bc4676834f4ed43ffd986152e1d2c
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-14 16:21:27 +0700

    Cấu trúc lại folder PUG

--- controller/client/topics.controller.ts
+++ controller/client/topics.controller.ts
@@ -3,8 +3,7 @@ import Topic from '../../model/topics.model';
 
 export const index = async(req: Request, res:Response)=>{
     const topics = await Topic.find({deleted: false});
-    console.log(topics);
     res.render('client/pages/topics/index', {
-        title:'Trang chủ đề bài hát'
+        title:'danh sách chủ đề'
     })
 }
