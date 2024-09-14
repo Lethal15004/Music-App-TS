2f06a566d55e50977cec1ac6da119bd6dae1c4c8
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-14 15:28:31 +0700

    Tạo route cơ bản và Cài đặt PUG

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log('OKE');
+import express,{Express, Request, Response} from 'express';
+import { title } from 'process';
+
+const app: Express = express();
+const port :number = 3000;
+
+
+
+//Phần view engine -> Để render file pug (Quan trọng phải có)
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+
+app.get('/topics',(req: Request, res:Response)=>{
+    res.render('client/pages/topics/index', {
+        title:'Trang chủ đề bài hát'
+    })
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
