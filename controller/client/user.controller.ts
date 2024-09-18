b0275d624d7c42dada0c52a5626dbbcf4e1d7c0c
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-18 17:28:01 +0700

    Sửa tính năng đăng ký và Thêm tính năng đăng nhập

--- controller/client/user.controller.ts
+++ controller/client/user.controller.ts
@@ -14,14 +14,23 @@ export const registerPage= async(req: Request, res: Response)=>{
 export const register= async(req: Request, res: Response)=>{
     if(!req.body.fullName || !req.body.email || !req.body.password){
         req.flash('error','Vui lòng nhập đầy đủ thông tin');
-        return res.redirect('/user/register');
+        return res.redirect('back');
     }
     const { fullName, email, password }: { fullName: string; email: string; password: string } = req.body;
+    const userExist = await User.findOne({
+        email:email,
+        deleted:false,
+        status:'active'
+    })
+    if(userExist){
+        req.flash('error','Tài khoản đã tồn tại');
+        return res.redirect('back');
+    }
     const data={
         fullName: fullName,
         email: email,
         password: md5(password),
-        token:generateHelper.generateRandomString(30)
+        tokenUser:generateHelper.generateRandomString(30)
     }
     const newUser = new User(data);
     await newUser.save();
@@ -30,5 +39,48 @@ export const register= async(req: Request, res: Response)=>{
     })
     await newListFavoriteSong.save();
     req.flash('success','Đăng ký thành công');
-    return res.redirect('/user/register');
+    res.redirect('/user/login');
+}
+
+export const loginPage= async(req: Request, res: Response)=>{
+    res.render('client/pages/user/login',{
+        title: 'Đăng nhập'
+    })
+}
+
+export const login= async(req: Request, res: Response)=>{
+    if(!req.body.email || !req.body.password){
+        req.flash('error','Vui lòng nhập đầy đủ thông tin');
+        return res.redirect('/user/login');
+    }
+    const { email, password }: { email: string; password: string } = req.body;
+    const user = await User.findOne({
+        email:email,
+        deleted:false,
+    })
+    if(!user){
+        req.flash('error','Tài khoản không tồn tại');
+        res.redirect('back');
+        return;
+    }
+    if(md5(password)!==user.password){
+        req.flash('error','Mật khẩu không chính xác');
+        res.redirect('back');
+        return;
+    }
+    if(user.status!=='active'){
+        req.flash('error','Tài khoản đang bị khóa');
+        res.redirect('back');
+        return;
+    }
+    res.cookie('tokenUser', user.tokenUser);
+    await User.updateOne({
+        email:email,
+        status:'active',
+        deleted:false
+    },{
+        statusOnline:'online'
+    })
+    req.flash('success','Đăng nhập thành công');
+    res.redirect('/topics');
 }
--- routes/client/user.route.ts
+++ routes/client/user.route.ts
@@ -4,6 +4,8 @@ const router= express.Router();
 import * as userController from '../../controller/client/user.controller';
 router.get('/register',userController.registerPage);
 router.post('/register',userController.register);
+router.get('/login',userController.loginPage);
+router.post('/login',userController.login);
 
 
 export default router;
